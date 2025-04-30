ec7c63bf92071c0bcd271461bd8f92ba12e68570
Marinaesra <Marina_esra@hotmail.com>
2025-04-30 19:10:49 +0200

    first

---
+++ app.js
@@ -0,0 +1,20 @@
+// GET -> GET PARA OBTENER RECURSOS -> find
+// POST -> POST PARA CREAR RECURSOS -> create
+// PUT -> PUT PARA ACTUALIZAR RECURSOS -> updateOne
+// DELETE -> DELETE PARA ELIMINAR RECURSOS -> deteleOne
+// PATCH -> PATCH PARA ACTUALIZAR PARCIALMENTE RECURSOS -> updateOne
+
+const PORT = 3000;
+const express = require('express');
+const userRouter = require('./routers/userRouter');
+ 
+const app = express();
+ 
+app.use('/api/user', userRouter);
+
+ 
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
+ 
+ 
