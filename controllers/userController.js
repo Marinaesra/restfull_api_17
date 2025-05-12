b7e33e40951243d2a4a30d6c7980110b82e713d7
Marinaesra <Marina_esra@hotmail.com>
2025-05-12 18:06:46 +0200

    primeras busquedas

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,14 +1,39 @@
 const users = require("../db/users");
-
-const getAllUser = (req, res)=>{
-    res.send(users);
+ 
+const getAllUser = (req, res) => {
+  res.send(users);
 };
-
+ 
 const getUserById = (req, res) => {
-    res.send(users[0]);
-  };
-
-module.exports ={
-    getAllUser,
-    getUserById,
+  const idUser  = req.params.idUser;
+  const user = users.find(u => u.id === parseInt(idUser));
+  if(!user) {
+    return res.status(200).send('No hay usuario')
+  }
+  res.send(user);
 };
+ 
+const getUserByName = (req, res) => {
+  const nombre  = req.params.nombre;
+  const user = users.filter(u => u.nombre.includes(nombre));
+  if(user.length === 0) {
+    return res.send('No hay usuarios');
+  }
+  res.send(user);
+};
+ 
+const addUser = (req, res) => {
+  const newUser = req.body;
+  console.log(newUser);
+  res.send('Usuario creado')
+};
+ 
+module.exports = {
+  getAllUser,
+  getUserById,
+  getUserByName,
+  addUser
+};
+ 
+ 
+ 
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,8 +1,14 @@
-const express = require ('express');
+const express = require("express");
 const router = express.Router();
-const {getAllUser, getUserById} = require ('../controllers/userController');
-
-router.get('/all-user', getAllUser);
-router.get('/:id', getUserById);
-
+const { getAllUser, getUserById , getUserByName , addUser } = require("../controllers/userController");
+ 
+router.get("/", getAllUser);
+router.post("/", addUser);
+router.get("/:idUser", getUserById);
+router.get("/searchName/:nombre", getUserByName);
+ 
 module.exports = router;
+ 
+// http://localhost:3000/api/user
+// http://localhost:3000/api/user/9
+// http://localhost:3000/api/user/searchName/Diego
